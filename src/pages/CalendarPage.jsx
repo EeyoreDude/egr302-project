8aec85c0bddd2f00070da3dcbe3a9a2dcaa2dce2
EeyoreDude <tylerjoseph.adcock@calbaptist.edu>
2022-03-16 15:02:36 -0700

    fixed typos and renamed calendar to calendarpage

---
+++ src/pages/CalendarPage.jsx
@@ -0,0 +1,18 @@
+function CalendarPage(){
+    return (
+        <>
+            <div className="activityStream">
+                <div class="container">
+                    <div class="row align-items-center my-5">
+                        <div class="col-lg-5">
+                            <h1 class="font-weight-light">Calendar</h1>
+                            <p>...</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default CalendarPage;
