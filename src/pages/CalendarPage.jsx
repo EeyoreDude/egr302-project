520f3a7f50461afae806af1884679cc2aa5b1e07
Jacob Eaton <jacobr.eaton@calbaptist.edu>
2022-03-27 23:07:19 -0700

    calendar exists

--- src/pages/CalendarPage.jsx
+++ src/pages/CalendarPage.jsx
@@ -1,3 +1,11 @@
+import React from 'react'
+import {Calendar, momentLocalizer} from 'react-big-calendar'
+import moment from 'moment'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+
+
+const localizer = momentLocalizer(moment)
+
 function CalendarPage(){
     return (
         <>
@@ -6,11 +14,20 @@ function CalendarPage(){
                     <div className="row align-items-center my-5">
                         <div className="col-lg-10">
                             <h1 className="font-weight-light">Calendar</h1>
-                            <p>...</p>
+                            <div className="calendarContainer">
+                            <Calendar
+                            localizer={localizer}
+                            defaultView = "month"
+                            />
+                            </div>
                         </div>
+                        
                     </div>
+                    
                 </div>
+                
             </div>
+            
         </>
     )
 }
