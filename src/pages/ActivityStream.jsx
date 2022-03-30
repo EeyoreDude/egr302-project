06d204e8744f9feab35f0c79ad275c1b25321103
erikameza <65696246+erikameza@users.noreply.github.com>
2022-03-30 00:06:50 -0700

    Fixed typo

--- src/pages/ActivityStream.jsx
+++ src/pages/ActivityStream.jsx
@@ -10,26 +10,26 @@ function ActivityStream(){
                 <div className="container">
                     <div className="row align-items-center my-5">
                         <div className="col-lg-10">
-                            <h1 className="font-weight-light">Activity Streamm </h1>
+                            <h1 className="font-weight-light">Activity Stream </h1>
                             <div class="one third column alpha"> 
                                 <h4 className="font-weight-light">Passed Due</h4>
                                 <div className="m-4 streamCard streamText" onClick={() => navigate('/assignment')}>
-                                    <p className="small float-right font-weight-light">due </p>
+                                    <p className="small float-right font-weight-light">due by -- </p>
                                     <h6>Assignment 1</h6>
                                     <p className="m-2 small w-100">Assignment discription</p>
                                     </div>
                             </div>
                          <div class="one third column ">
-                            <h4 className="font-weight-light">Today</h4>
+                            <h4 className="font-weight-light">Due Today</h4>
                                 <div className="m-4 streamCard streamText" onClick={() => navigate('/assignment')}>
-                                    <p className="small float-right font-weight-light">due </p>
+                                    <p className="small float-right font-weight-light">due by -- </p>
                                     <h6>Assignment 2</h6>
                                     <p className="m-2 small w-100">Assignment discription</p>
                             </div>
                         </div>
-                            <h4 className="font-weight-light">Tomorrow</h4>
+                            <h4 className="font-weight-light">Due Tomorrow</h4>
                                 <div className=" m-4 streamCard streamText" onClick={() => navigate('/assignment')}>
-                                    <p className="small float-right font-weight-light">due </p>
+                                    <p className="small float-right font-weight-light">due by --</p>
                                     <h6>Assignment 3</h6>
                                     <p className="m-2 small w-100">Assignment discription</p>
                                 </div>
