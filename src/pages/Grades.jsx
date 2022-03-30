18401cf4134ad42a8c86c26b5a8f7b3c3ce37149
erikameza <65696246+erikameza@users.noreply.github.com>
2022-03-30 00:07:25 -0700

    Grades page playout created

--- src/pages/Grades.jsx
+++ src/pages/Grades.jsx
@@ -1,4 +1,7 @@
 import { useNavigate } from "react-router-dom"
+import React from "react";
+import {getAuth} from "firebase/auth"
+import {useEffect, useState} from "react"
 
 function Grades(){
     const navigate = useNavigate()
@@ -8,32 +11,33 @@ function Grades(){
                 <div class="container">
                     <div class="row align-items-center my-5">
                         <div class="col-lg-5">
-                            <h1 class="font-weight-light">Grades</h1>
-                            
+                            <h1 class="font-weight-light">Grades for </h1>
+                            <div id="content-wrapper" class="ic-Layout-contentWrapper">
 
                             <div className="pageLayout">
-                                <tr>
+                            <tr>
                             <th scope="col"> Name</th>
                             <th scope="col"> Due</th>
                             <th scope="col" class="status">Status</th>
                             <th scope="col" class="assignment_score"> Score</th>
                             <th scope="col" class="possible"> Out of</th>
                             </tr>
-                            <h6 className="font-weight-light">Assignment 1</h6>
+                            <td className="font-weight-light">Assignment 1 </td>
                                 <div className="m-4 streamCard streamText" onClick={() => navigate('/assignment')}>
                                     <p className="small float-right font-weight-light">100</p>
                                     
                                     
                             </div>
-                            <h6 className="font-weight-light">Assignment 2</h6>
+                            <td className="font-weight-light">Assignment 2</td>
                                 <div className=" m-4 streamCard streamText" onClick={() => navigate('/assignment')}>
                                     <p className="small float-right font-weight-light">--%</p>
                                 
                                 </div>
-                             <h6 className="font-weight-light">Assignment 3</h6>
+                             <td className="font-weight-light">Assignment 3</td>
                                 <div className=" m-4 streamCard streamText" onClick={() => navigate('/assignment')}>
                                     <p className="small float-right font-weight-light">--%</p>
                                     
+                                </div>
                                 </div>
                             </div>
                         </div>
