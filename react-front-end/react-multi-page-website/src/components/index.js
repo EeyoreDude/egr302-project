c07ec83a433e748ff898fbdbf2e135daea0db213
Tyler Adcock <tylerjoseph.adcock@calbaptist.edu>
2022-04-25 02:39:21 -0700

    fixed stupid bug with files

--- react-front-end/react-multi-page-website/src/components/index.js
+++
@@ -1,8 +0,0 @@
-export { default as Navigation } from "./Navigation";
-export { default as Footer } from "./Footer";
-export { default as Home } from "./Home";
-export { default as Grades } from "./Grades";
-export { default as Calendar } from "./Calendar";
-export { default as ActivityStream } from "./blog/ActivityStream";
-export { default as Assignments } from "./blog/Assignments";
-export { default as Assignment } from "./blog/Assignment";
