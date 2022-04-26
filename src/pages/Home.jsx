fb9ce0f9ed0b1770a569cee2dc394c0f0f2d29cb
Tyler Adcock <tylerjoseph.adcock@calbaptist.edu>
2022-04-25 17:22:50 -0700

    a couple visual overrides for the final video

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,52 +1,54 @@
 import React from "react"
-import {getAuth} from "firebase/auth"
+import { getAuth } from "firebase/auth"
 import { useNavigate } from 'react-router-dom'
 import { useEffect, useState } from "react"
 import { toast } from "react-toastify";
 import CourseDisplay from "./components/CourseDisplay";
 
-export default function Home({events, Courses}) {
+export default function Home({ events, Courses }) {
 
-    const upcoming = events.filter((item) => item.start.getDate() >  new Date().getDate() &&
-                                                item.start.getMonth() >=  new Date().getMonth() &&
-                                                item.start.getFullYear() ===  new Date().getFullYear())
+    const upcoming = events.filter((item) => item.start.getDate() > new Date().getDate() &&
+        item.start.getMonth() >= new Date().getMonth() &&
+        item.start.getFullYear() === new Date().getFullYear())
 
-	const [user,setUser] = useState({})
-	
-	const auth = getAuth()
+    const [user, setUser] = useState({})
+
+    const auth = getAuth()
     const navigate = useNavigate()
 
-	useEffect(() => {
-        if(auth.currentUser === null){
+    useEffect(() => {
+        if (auth.currentUser === null) {
             navigate('/profile')
             toast.error("Please sign in.")
         } else {
-    		setUser(auth.currentUser)
+            setUser(auth.currentUser)
         }
-	}, [auth.currentUser])
+    }, [auth.currentUser])
 
-	return (
+    return (
         <>
             <div className="pageLayout">
                 <div className="container">
                     <div className="row align-items-center my-5">
                         <div className="col-8 h-100">
                             <h1 className="font-weight-light">{user ? user.displayName : 'please sign in or register to use most features'}'s Courses</h1>
-                            <CourseDisplay Courses={Courses}/>
+                            <CourseDisplay Courses={Courses} />
                         </div>
                         <div className="col top-align h-100">
-                                <h1 className="font-weight-light">Upcoming</h1>
-                                <ul>
-                                {upcoming.map((item)=>(
-                                    <li className="upcomingText" onClick={ () => navigate('course/' + item.course + '/' + item.id)}>
-                                        {item.title}
-                                    </li>
-                                ))}
-                                </ul>
-                            </div>
+                            <h1 className="font-weight-light">Upcoming</h1>
+                            <ul>
+                                {
+                                    upcoming.map((item) => (
+                                        <li className="upcomingText" onClick={() => navigate('course/' + item.course + '/' + item.id)}>
+                                            {item.title}
+                                        </li>
+                                    ))
+                                }
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
         </>
-	);
+    );
 }
--- src/pages/components/CourseCard.jsx
+++ src/pages/components/CourseCard.jsx
@@ -9,13 +9,16 @@ function CourseCard({course}){
     //for navigation
     const navigate = useNavigate()
 
-    const url = '/grades/' + cardCourse.semester + '/' + cardCourse.name
+    const semester = cardCourse.semester = undefined ? 'SP2022' : cardCourse.semester;
+    const name = cardCourse.name = 'TestClass' ? 'EGR302' : cardCourse.name;
+
+    const url = '/grades/' + semester + '/' + cardCourse.name
 
         return (
             <div className="m-4 streamCard streamCardBgNormal streamText" onClick={() => navigate(url)}>
             <p className="small float-right font-weight-light">Section: {course.section}</p>
             <h6>{course.name}</h6>
-            <p className="m-2 small w-100">{course.description}</p>
+            <p className="m-2 small w-100">{cardCourse.description = undefined ? 'A description of the course!' : cardCourse.description}</p>
             </div>
         )
     }
